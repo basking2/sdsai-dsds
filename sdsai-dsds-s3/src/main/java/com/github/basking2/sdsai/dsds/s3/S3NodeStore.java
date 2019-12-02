e46d4f68663687dd7947d26cace4e7c05e8cf198
Sam Baskinger <basking2@yahoo.com>
2019-12-01 21:16:03 -0600

    S3NodeStore.java: Correctly implement the S3 storage layer.

--- sdsai-dsds-s3/src/main/java/com/github/basking2/sdsai/dsds/s3/S3NodeStore.java
+++ sdsai-dsds-s3/src/main/java/com/github/basking2/sdsai/dsds/s3/S3NodeStore.java
@@ -5,21 +5,51 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
 import com.amazonaws.util.IOUtils;
 import com.github.basking2.sdsai.dsds.node.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.UUID;
 
 public class S3NodeStore implements NodeStore<String, S3ObjectId, String> {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(S3NodeStore.class);
+
+    /**
+     * How S3 is accessed.
+     */
     private final AmazonS3 client;
+
+    /**
+     * The prefix all keys will be placed under.
+     */
     private final String prefix;
+
+    /**
+     * The bucket keys will in.
+     */
     private final String bucket;
 
+    /**
+     * Constructor.
+     * @param client How to access S3.
+     * @param bucket The bucket.
+     * @param prefix The key prefix that keys will be put under. This should probably end in a / as no / is inserted.
+     */
     public S3NodeStore(final AmazonS3 client, final String bucket, final String prefix) {
         this.client = client;
         this.bucket = bucket;
         this.prefix = prefix;
     }
 
+    /**
+     * Use the default S3 client but otherwise the same constructor.
+     * @param bucket The bucket.
+     * @param prefix The key prefix that keys will be put under. This should probably end in a / as no / is inserted.
+     */
     public S3NodeStore(final String bucket, final String prefix) {
         this(AmazonS3ClientBuilder.defaultClient(), bucket, prefix);
     }
@@ -37,7 +67,7 @@ public class S3NodeStore implements NodeStore<String, S3ObjectId, String> {
         }
         catch(final AmazonS3Exception e) {
             if (e.getErrorCode().equals("NoSuchKey")) {
-                throw new NodeStoreNodeNotFoundException(e);
+                return null;
             }
             else {
                 throw new NodeStoreException("Loading key "+prefix+key.getKey(), e);
@@ -82,7 +112,11 @@ public class S3NodeStore implements NodeStore<String, S3ObjectId, String> {
                 public void close() throws IOException {
                     final ObjectMetadata objectMetadata = new ObjectMetadata();
                     objectMetadata.setContentLength(count);
-                    final PutObjectRequest putObjectRequest = new PutObjectRequest(key.getBucket(), prefix + key.getKey(), new ByteArrayInputStream(buf, 0, count), objectMetadata);
+                    final PutObjectRequest putObjectRequest = new PutObjectRequest(
+                            key.getBucket(),
+                            prefix + key.getKey(),
+                            new ByteArrayInputStream(buf, 0, count),
+                            objectMetadata);
                     client.putObject(putObjectRequest);
                 }
             };
